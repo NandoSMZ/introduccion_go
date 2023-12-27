78b089ca6cc5bc2d7fa945fc3841c1df6d3284b2
fernando.salazar <fernando.salazar@rappi.com>
2023-12-27 10:04:59 -0500

    condiciones_with_task

--- src/main.go
+++ src/main.go
@@ -33,4 +33,24 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println("Value:", value)
+
+	//Reto de saber si uyn numero es par y de verificar password
+	if isPair(6) {
+		fmt.Println("Number is pair")
+	} else {
+		fmt.Println("Number is odd")
+	}
+	if isValidUser("Alpha5", "MyPassword") {
+		fmt.Println("Credentials are valid")
+	} else {
+		fmt.Println("Credentials aren't valid")
+	}
+}
+
+func isPair(num int) bool {
+	return num%2 == 0
+}
+
+func isValidUser(userName, pass string) bool {
+	return userName == "Alpha" && pass == "MyPassword"
 }
