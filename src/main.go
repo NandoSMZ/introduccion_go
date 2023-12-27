ecd3404821f37c7a50e47d5984471704ec101794
fernando.salazar <fernando.salazar@rappi.com>
2023-12-27 10:36:24 -0500

    condiciones_Switch

--- src/main.go
+++ src/main.go
@@ -1,56 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"strconv"
-)
+import "fmt"
 
 func main() {
-	valor1 := 1
-	valor2 := 2
-
-	if valor1 == 1 {
-		fmt.Println("Es 1")
-	} else {
-		fmt.Println("No es 1")
+	modulo := 5 % 2
+	switch modulo {
+	case 0:
+		fmt.Println("Es par")
+	default:
+		fmt.Println("Es Impar")
 	}
-
-	//With and
-	if valor1 == 1 && valor2 == 2 {
-		fmt.Println("Es Verdad")
-	}
-
-	//With or
-	if valor1 == 1 || valor2 == 2 {
-		fmt.Println("Es Verdad")
-	}
-
-	//Convertir texto a numero
-	value, err := strconv.Atoi("53")
-	//Si no existe error va ser nil
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Value:", value)
-
-	//Reto de saber si uyn numero es par y de verificar password
-	if isPair(6) {
-		fmt.Println("Number is pair")
-	} else {
-		fmt.Println("Number is odd")
-	}
-	if isValidUser("Alpha5", "MyPassword") {
-		fmt.Println("Credentials are valid")
-	} else {
-		fmt.Println("Credentials aren't valid")
-	}
-}
-
-func isPair(num int) bool {
-	return num%2 == 0
-}
-
-func isValidUser(userName, pass string) bool {
-	return userName == "Alpha" && pass == "MyPassword"
 }
