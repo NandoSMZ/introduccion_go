c2a6b8a511066b4dae726472484ea2f97fba2fb9
fernando.salazar <fernando.salazar@rappi.com>
2023-12-27 12:10:48 -0500

    keyword_defer_break_continue

--- src/main.go
+++ src/main.go
@@ -3,22 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	//Se define la variable y luego se hace el parceo de esa variable o variable a evaluar
-	switch modulo := 5 % 2; modulo {
-	case 0:
-		fmt.Println("Es par")
-	default:
-		fmt.Println("Es Impar")
-	}
+	//Defer hace que se ejecute como la ultima linea
+	// Es decir que primero imprime "Mundo" y luego "Hola"
+	defer fmt.Println("Hola")
+	fmt.Println("Mundo")
 
-	//sin condicion
-	value := 200
-	switch {
-	case value > 100:
-		fmt.Println("Es mayor a 100")
-	case value < 0:
-		fmt.Println("Es menor a 0")
-	default:
-		fmt.Println("No Condicion")
+	// continue y Break
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+
+		//continue, sirve cuando hay errores
+		if i == 2 {
+			fmt.Println("Es 2")
+			continue
+		}
+
+		//Break
+		if i == 6 {
+			fmt.Println("Es 6 BREAK")
+			break
+		}
 	}
 }
