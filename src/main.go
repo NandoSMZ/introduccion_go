bc98feb1449765bb2a00f94563c72cab406532c1
fernando.salazar <fernando.salazar@rappi.com>
2023-12-27 09:43:20 -0500

    ciclo_for

--- src/main.go
+++ src/main.go
@@ -1,5 +1,26 @@
 package main
 
+import "fmt"
+
 func main() {
+	// En GO solo se usa el ciclo "FOR"
+	// For condicional
+	for i := 0; i <= 10; i++ {
+		fmt.Println(i)
+	}
+
+	// For tipo While
+	counter := 0
+	for counter < 10 {
+		fmt.Println(counter)
+		counter++
+	}
+
+	//For forever
+	counterForever := 0
+	for {
+		fmt.Println(counterForever)
+		counterForever++
+	}
 
 }
