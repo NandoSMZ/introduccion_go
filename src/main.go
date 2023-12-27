8c1122fa51aa1ba8f0f753f043bd5792633812a3
fernando.salazar <fernando.salazar@rappi.com>
2023-12-27 10:36:31 -0500

    condiciones_Switch1

--- src/main.go
+++ src/main.go
@@ -3,11 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	modulo := 5 % 2
-	switch modulo {
+	//Se define la variable y luego se hace el parceo de esa variable o variable a evaluar
+	switch modulo := 5 % 2; modulo {
 	case 0:
 		fmt.Println("Es par")
 	default:
 		fmt.Println("Es Impar")
+	}
+
+	//sin condicion
+	value := 200
+	switch {
+	case value > 100:
+		fmt.Println("Es mayor a 100")
+	case value < 0:
+		fmt.Println("Es menor a 0")
+	default:
+		fmt.Println("No Condicion")
 	}
 }
