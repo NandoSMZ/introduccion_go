8899b9cc0db33bdffa7358e5a89047d5c7337da0
fernando.salazar <fernando.salazar@rappi.com>
2023-12-28 10:53:52 -0500

    maps

--- src/main.go
+++ src/main.go
@@ -2,45 +2,24 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-func isPalindromo(text string) {
-	var textReverse string
-
-	for i := len(text) - 1; i >= 0; i-- {
-		textReverse += string(text[i])
-	}
-
-	if text == textReverse {
-		fmt.Println("Es Palindomo")
-	} else {
-		fmt.Println("No es Palindromo por que la palabra es:", textReverse)
-	}
-}
-
 func main() {
-	slice := []string{"Hola", "Mundo", "Que Hace"}
+	m := make(map[string]int)
 
-	//Para traer indice y valor
-	for i, valor := range slice {
-		fmt.Println(i, valor)
+	m["Jose"] = 14
+	m["Pepito"] = 20
+
+	fmt.Println(m)
+
+	//Recorrer map
+
+	for i, v := range m {
+		fmt.Println(i, v)
 	}
 
-	// Solo necesito el valor
-	for _, valor := range slice {
-		fmt.Println(valor)
-	}
-
-	// Solo indice
-	for i := range slice {
-		fmt.Println(i)
-	}
-
-	var palabra string
-	fmt.Println("Ingrese la palabra")
-	fmt.Scan(&palabra)
-	palabraToMinus := strings.ToLower(palabra)
-	fmt.Println("Llamando la funcion para la palabra:", palabra)
-	isPalindromo(palabraToMinus)
+	// Encontrar Valor
+	// con el ok, vamos a saber si la variable existe y sera booleano
+	value, ok := m["Jose"]
+	fmt.Println(value, ok)
 }
