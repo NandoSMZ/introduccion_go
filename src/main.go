9ff388b33b57ecfb3ef1df3f58a74e0c41d622a5
fernando.salazar <fernando.salazar@rappi.com>
2023-12-29 11:12:55 -0500

    Channels_Range_Close_Select

--- src/main.go
+++ src/main.go
@@ -2,17 +2,36 @@ package main
 
 import "fmt"
 
-func say(text string, c chan string) {
-	c <- text //Vamos a ingresar el text al chanel
+func message(text string, c chan string) {
+	c <- text
 }
 
 func main() {
-	//creacion de chanel especificando cuantas goroutines van a pasar en esste caso "1" si se deja basio sera default o dinamico
-	c := make(chan string, 1)
+	c := make(chan string, 2) // canal con capacidad maxima de 2
+	c <- "mensaje 1"
+	c <- "Mensaje 2"
 
-	fmt.Println("Hello")
+	fmt.Println(len(c), cap(c)) // len para saber la cantidad de datos y cap para la capacidad del canal
 
-	go say("Bye", c) //ejecutamos la funcion indicando el chanel c
+	//Range y Close
 
-	fmt.Println(<-c) // Sacamos el dato que esta en el channel
+	close(c) // Va cerrar el canal, es decir que no va recibir mas datos el cnal
+
+	for message := range c {
+		fmt.Println(message)
+	}
+
+	//select
+	email1 := make(chan string) //Canal dinamico
+	email2 := make(chan string)
+	go message("Mensaje 1 Email1", email1)
+	go message("Mensaje 2 Email2", email2)
+	for i := 0; i < 2; i++ {
+		select {
+		case m1 := <-email1:
+			fmt.Println("Email Recibido de email1", m1)
+		case m2 := <-email2:
+			fmt.Println("Email Recibido de email2", m2)
+		}
+	}
 }
